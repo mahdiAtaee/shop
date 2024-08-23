04372b45b628c2d7aae71b80fad1f4a9884dc21e
mahdi <mahdiataee1689@gmail.com>
2024-08-23 22:06:17 +0330

    init-commit

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import * as express from "express";
+import { Application } from "express";
+import Router from "./router";
+
+class App {
+  public app: Application;
+  public port: number;
+  private router: Router;
+  constructor(port: number) {
+    this.app = express();
+    this.port = port;
+    this.router = new Router(this.app);
+    this.router.run();
+  }
+  public start() {
+    this.app.listen(this.port, () => {
+      console.log("app is runnig on port", this.port);
+    });
+  }
+}
+
+export default App;
